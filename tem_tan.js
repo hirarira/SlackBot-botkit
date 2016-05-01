a40d0346b85f244a02e0a4abdd640751545f53c4
hirarira <hirarira617@gmail.com>
2016-05-01 12:34:16 +0900

    乗換案内Lib追加

--- tem_tan.js
+++ tem_tan.js
@@ -259,7 +259,7 @@ controller.hears(['おしえて','教えて'], ['ambient'], function(bot, messag
                 default: true,
                 callback: function(response, convo) {
 					var res = response.text;
-					if(res.match(/[知し]って(る|いる)*|把握*|存知*|存じて*|はい/)){
+					if(res.match(/[知し]って(る|いる)|把握|存知|存じて|はい/)){
 						convo.say("へぇ〜お兄ちゃんは博識だねー！");
 		                convo.next();
 					}
@@ -270,10 +270,13 @@ controller.hears(['おしえて','教えて'], ['ambient'], function(bot, messag
 							if (!error && response.statusCode == 200) {
 								console.log(body);
 								var out_str = "もー仕方ないなーお兄ちゃんは…これのことだよ！\n";
-								out_str += "https://ja.wikipedia.org/wiki/";
-								out_str += body;
-								convo.say(out_str);
-								convo.next();
+								var in_url = "https://ja.wikipedia.org/wiki/" + body;
+								ShortURL(in_url,function(body){
+									console.log("ShortURL:"+body.id);
+									out_str += body.id;
+									convo.say(out_str);
+									convo.next();
+								});
 							} else {
 								console.log('error: '+ response.statusCode);
 								convo.say('error: '+ response.statusCode);
@@ -358,7 +361,7 @@ controller.hears(['マインスイーパ','mine'], ['ambient'], function(bot, me
 })
 // 乗換検索
 controller.hears(['乗り換え','乗換'], ['ambient'], function(bot, message) {
-	
+	Norikae(bot,message);
 })
 controller.hears(['じゃんけん','ジャンケン'], ['ambient'], function(bot, message) {
 		console.log("USER:"+message.user);
@@ -503,7 +506,126 @@ function getSlackUser(bot,message,callback){
 		}
 	});
 }
-
+// 短縮URL化に必要なトークンを取ってくるだけ
+function GetShortURLToken(){
+	var fs = require('fs');
+	var buf = fs.readFileSync("./var/token.txt");
+	var token = buf.toString();
+	return token;
+}
+// 短縮URLを作成する関数
+function ShortURL(in_url,callback){
+	var token = GetShortURLToken();
+	var request = require('request');
+	var set_url = "https://www.googleapis.com/urlshortener/v1/url?key=" + token;
+	var send_option = {
+		url : set_url,
+		headers:{
+			'Content-Type': 'application/json'
+		},
+		json : true,
+		body :{
+            longUrl: in_url
+        }
+	};
+	request.post(send_option,function(error, response, body){
+		if (!error && response.statusCode == 200) {
+			console.log('OK: '+ response.statusCode);
+			callback(body);
+		}
+		else{
+			console.log('error: '+ response.statusCode);
+		}
+	});
+}
+// 自然数かつ10000未満の値か判定
+function isInteger(x){
+	if(isNaN(x))return false;
+	if(isFinite(x))return false;
+	if(x <= 0 || x >= 10000)return false;
+	return Math.floor(x) === x;
+}
+// 乗り換え案内
+function Norikae(bot,message){
+	var in_str = message.text;
+	var out_str = "";
+	var in_url = "http://www.navitime.co.jp/transfer/searchlist?&basis=1&orvStationName=";
+	var NorikaeST = in_str.split(/ |　/);
+	if(NorikaeST.length >= 3){
+		var StartST = NorikaeST[1];
+		var EndST = NorikaeST[2];
+		out_str = StartST + "駅 から" + EndST + "駅 までの乗換経路はこんな感じだよ！\n";
+		in_url += StartST + "&dnvStationName=" + EndST;
+		if(NorikaeST.length >= 4){
+			var NowDate = new Date();
+			// N分前にマッチ
+			if(in_str.match(/前|(B|b)efore/)){
+				
+			}
+			else if(in_str.match(/後|(A|a)fter/)){
+				
+			}
+			else{
+				var settimeOK = true;
+				// 10:00 時刻のみ
+				if(NorikaeST.length == 4){
+					var inHM = NorikaeST[3].split(":");
+				}
+				else{
+					// 2010/01/01 10:20 フル形式
+					var inYMD = NorikaeST[3].split("/");
+					var inHM = NorikaeST[4].split(":");
+					// 2要素以上あるかチェック
+					settimeOK = (inYMD.length >= 2)?true:false;
+					for(var i=0;i<inYMD.length;i++){
+						inYMD[i] = Number(inYMD[i]);
+						// 自然数判定
+						if(!isInteger(inYMD[i])){
+							settimeOK = false;
+						}
+						console.log(inYMD[i]);
+					}
+					if(settimeOK){
+						// 04/06 などの月日のみパターン
+						if(inYMD.length == 2){
+							in_url += "&month="+ inYMD[0];
+							in_url += "&day=" + inYMD[1];
+						}
+						else{
+							in_url += "&month="+ inYMD[0] +"%2F" + inYMD[1];
+							in_url += "&day=" + inYMD[2];
+						}
+					}
+				}
+				settimeOK = true;
+				// 時、分が揃っているかチェック
+				settimeOK = (inHM.length >= 2)?true:false;
+				for(var i=0;i<inHM.length;i++){
+					inHM[i] = Number(inHM[i]);
+					if(!isInteger(inHM[i])){
+						settimeOK = false;
+					}
+					console.log(inHM[i]);
+				}
+				if(settimeOK){
+					in_url += "&hour=" + inHM[0] + "&minute=" + inHM[1];
+					console.log("OK!");
+				}
+			}
+		}
+		console.log(in_url);
+		// 出来たURLを短縮化
+		ShortURL(in_url,function(body){
+			console.log("ShortURL:"+body.id);
+			out_str += body.id;
+			bot.reply(message,out_str);
+		});
+	}
+	else{
+		console.log("入力エラー");
+		bot.reply(message,"お兄ちゃん！\n乗換 {出発駅} {目的駅} 年/月/日 時間:分\nの形式で入力してね！");
+	}
+}
 // Wikipediaランダム
 function get_wiki_rand(message){
 	var get_url = "https://ja.wikipedia.org/wiki/%E7%89%B9%E5%88%A5:%E3%81%8A%E3%81%BE%E3%81%8B%E3%81%9B%E8%A1%A8%E7%A4%BA"
@@ -523,16 +645,17 @@ function get_wiki_rand(message){
 	// DBに登録
 	var request = require('request');
 	var options = {
-	  uri: "http://localhost/ajax_test/wiki_add.php",
-	  form: { in_value: get_title },
-	  json: true
+		uri: "http://localhost/ajax_test/wiki_add.php",
+		form: { in_value: get_title },
+		json: true
 	};
 	request.post(options, function(error, response, body){
-	  if (!error && response.statusCode == 200) {
-		console.log(body);
-	  } else {
-		console.log('error: '+ response.statusCode);
-	  }
+		if (!error && response.statusCode == 200) {
+			console.log(body);
+		}
+		else{
+			console.log('error: '+ response.statusCode);
+		}
 	});
 	// DB登録ここまで
 	return out_str;
